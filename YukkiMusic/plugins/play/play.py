8b60b70642894f9d16843b79df862075c6175501
RexAshh <95988728+Rexashh@users.noreply.github.com>
2022-06-01 22:29:59 +0700

    Update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -34,7 +34,7 @@ from YukkiMusic.utils.inline.play import (livestream_markup,
 from YukkiMusic.utils.inline.playlist import botplaylist_markup
 from YukkiMusic.utils.logger import play_logs
 from YukkiMusic.utils.stream.stream import stream
-from YukkiMusic.utils.sub import ken
+
 
 # Command
 PLAY_COMMAND = get_command("PLAY_COMMAND")
@@ -46,7 +46,6 @@ PLAY_COMMAND = get_command("PLAY_COMMAND")
     & ~filters.edited
     & ~BANNED_USERS
 )
-@ken
 @PlayWrapper
 async def play_commnd(
     client,
@@ -57,6 +56,7 @@ async def play_commnd(
     channel,
     playmode,
     url,
+    fplay,
 ):
     mystic = await message.reply_text(
         _["play_2"].format(channel) if channel else _["play_1"]
